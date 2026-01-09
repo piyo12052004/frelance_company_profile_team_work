cad7306232d7884d9b926ad9eedf9e8ea8152783
faisal <ahmadfaisalazizi12@gmail.com>
2026-01-09 11:08:32 +0700

    a

--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -53,6 +53,7 @@
 
 
     <!-- Project Start -->
+
     <div class="container-fluid project py-5 my-5">
         <div class="container py-5">
             <div class="text-center mx-auto pb-5 wow fadeIn" data-wow-delay=".3s" style="max-width: 600px;">
